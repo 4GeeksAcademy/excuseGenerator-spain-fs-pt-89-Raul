d2af6147d7eabb94a9813d711366904d6a5bc43b
RaulSalas23 <rast2305@gmail.com>
2024-11-22 19:46:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //aleatoriedad
+
+  let arraywho = Math.floor(Math.random() * who.length);
+  let arrayaction = Math.floor(Math.random() * action.length);
+  let arraywhat = Math.floor(Math.random() * what.length);
+  let arraywhen = Math.floor(Math.random() * when.length);
+
+  //excusa
+  document.querySelector("#excuse").innerHTML =
+    who[arraywho] +
+    " " +
+    action[arrayaction] +
+    " " +
+    what[arraywhat] +
+    " " +
+    when[arraywhen];
 };
